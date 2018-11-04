ce62c8e656fdea727156c0a0a89f1700dd5b14c3
Chris Zubak-Skees <chriszs@gmail.com>
2018-11-03 22:31:33 -0400

    Very painfully deal with unescaped quotes

--- pa/load.sh
+++ pa/load.sh
@@ -8,6 +8,12 @@ for year in 2018 2017 2016 2015 2014 2013; do
     for file in $(\ls -d *_$year_*.txt); do
     	mv $file $year/$(echo $file | sed -e 's/\(.*\)_'$year'_.*\.txt$/\1_'$year'.txt/')
     done
+    # try to fix unescaped quotes
+    for file in $(\ls $year/*.txt); do
+    	iconv -f WINDOWS-1252 -t UTF-8 $file | tr -d '\r' > $file.converted
+    	sed -e 's/\([^,]\)\"\([^,]\)/\1\"\"\2/g' $file.converted | sed -e 's/\(\"\"[^\",]\)\"\([^,]\)/\1\"\"\2/g' | sed -e 's/\(,"[^,]*[^"],\)\"\([^",]\)/\1\"\"\2/g' > $file
+    	rm $file.converted
+    done
 	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_contributions FROM '"$year"/contrib_"$year".txt' WITH CSV;"
 	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_debts FROM '"$year"/debt_"$year".txt' WITH CSV;"
 	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_expenditures FROM '"$year"/expense_"$year".txt' WITH CSV;"
