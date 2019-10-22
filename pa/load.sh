78810f5ff6176cca7df00023d029f5667c4738d1
Chris Zubak-Skees <chriszs@gmail.com>
2019-10-22 12:27:23 -0400

    Remove CPI specific database info for PA
    Use PG env vars instead

--- pa/load.sh
+++ pa/load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -U politics -h db.fivetwentyseven.com politics -f create.sql
+psql -f create.sql
 for year in 2018 2017 2016 2015 2014 2013; do
     wget -N "https://www.dos.pa.gov/VotingElections/CandidatesCommittees/CampaignFinance/Resources/Documents/"$year".zip"
     unzip -o $year".zip"
@@ -14,10 +14,10 @@ for year in 2018 2017 2016 2015 2014 2013; do
     	sed -e 's/\([^,]\)\"\([^,]\)/\1\"\"\2/g' $file.converted | sed -e 's/\(\"\"[^\",]\)\"\([^,]\)/\1\"\"\2/g' | sed -e 's/\(,"[^,]*[^"],\)\"\([^",]\)/\1\"\"\2/g' > $file
     	rm $file.converted
     done
-	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_contributions FROM '"$year"/contrib_"$year".txt' WITH CSV;"
-	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_debts FROM '"$year"/debt_"$year".txt' WITH CSV;"
-	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_expenditures FROM '"$year"/expense_"$year".txt' WITH CSV;"
-	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_filers FROM '"$year"/filer_"$year".txt' WITH CSV;"
-	psql -U politics -h db.fivetwentyseven.com politics -c "\COPY pa_other_receipts FROM '"$year"/receipt_"$year".txt' WITH CSV;"
+	psql -c "\COPY pa_contributions FROM '"$year"/contrib_"$year".txt' WITH CSV;"
+	psql -c "\COPY pa_debts FROM '"$year"/debt_"$year".txt' WITH CSV;"
+	psql -c "\COPY pa_expenditures FROM '"$year"/expense_"$year".txt' WITH CSV;"
+	psql -c "\COPY pa_filers FROM '"$year"/filer_"$year".txt' WITH CSV;"
+	psql -c "\COPY pa_other_receipts FROM '"$year"/receipt_"$year".txt' WITH CSV;"
     rm -rf $year
 done
